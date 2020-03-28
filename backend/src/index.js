6b31aabcfcbad6be70cf586a4f68309e60af8a88
Gilson Santos <gilsonjosert@gmail.com>
2020-03-28 00:50:25 +0000

    celebrate install

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 const port = 3333;
 const app = express();
 
 app.use(cors()); // Liberando para todas as aplicações acessarem o back-end ou podemos determina uma url especifica que pode acessar o recurso.
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(port);
